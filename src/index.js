64e8810835e55cbfa821eff6c9d87ec1e01340d4
VyelenTheGreat <risalydavid@gmail.com>
2025-04-02 10:34:09 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,14 @@ import {
     DisconnectReason, 
     useMultiFileAuthState
 } from "@whiskeysockets/baileys";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { config } from "dotenv";
-import commandHandler from "./commands.js";
 import pino from "pino";
 
 config();
 
-const client = new GoogleGenerativeAI(process.env.API_KEY).getGenerativeModel({ 
-    model: "gemini-1.5-flash"
-});
 const connect = async () => {
     const { state, saveCreds } = await useMultiFileAuthState("auth_info");
     const sock = makeWASocket({
         printQRInTerminal: true, 
-        version: [22, 13, 74],
         browser: Browsers.ubuntu("Chrome"),
         auth: state,
         logger: pino({ level: "fatal" })
@@ -26,16 +19,14 @@ const connect = async () => {
 
     sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
         if(
-        
             connection === "close" && 
             lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut
-        
         ) connect();
     });
     sock.ev.on ("creds.update", saveCreds);
     
     sock.ev.on("messages.upsert", async msg => {
-        await commandHandler(sock, client, msg)
+        // this event used when a message was sent
     });
 };
 connect();
